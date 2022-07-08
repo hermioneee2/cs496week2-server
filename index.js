06a5c63d3b8b9f319871e69fa7f4a9d823390c9f
taeyoung-1 <hermioneee2@gmail.com>
2022-07-08 14:36:28 +0000

    Returning path object from DB

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-// const express = require('express')
-// const app = express()
-
-// const hostname = '172.10.5.98'
-// const port = 443
-
-// app.get('/', async (req, res) => {
-//   res.send("hihi")
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`)}
-// );
-
-const http = require('http')
+const express = require('express')
+const url = require('url')
+const bodyParser = require('body-parser')
+const querystring = require('querystring')
+const app = express()
+app.use(bodyParser.urlencoded( { extended : false }))
+app.use(bodyParser.json())
 
 const hostname = '172.10.5.98'
 const port = 443
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
-})
+// neo4j
+const neo4j = require('neo4j-driver')
+const personName = 'Taeyoung'
 
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+app.get('/:id', async (req, res) => {
+  let newUser = req.query.newUser
+  let id = req.params.id
+
+  console.log(newUser.toString())
+  console.log(id)
+
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+
+  const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "lhryty2"))
+  const session = driver.session()
+  try {
+    const result = await session.run(
+      'MATCH path = (p {name : $name})-[r]-(q) RETURN p,r,q,path',
+      { name: personName }
+    )
+
+    const record = result.records[0]
+    const path = record.get('path')
+  
+    res.json({
+      start: path.start,
+      end: path.end,
+      length: path.length
+    })
+  } catch (e) {
+    res.end(e.toString())
+  } finally {
+    await session.close()
+  }
+  
+  // on application exit:
+  await driver.close()
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)}
+);
